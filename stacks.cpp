970b6ce74f48d0866931f87dd4f624be7d734b87
Yogesh Narendra Tiwari <92584567+yogeshnarendratiwari@users.noreply.github.com>
2021-10-16 13:56:29 +0530

    Full code for Stacks ADT

--- stacks.cpp
+++ stacks.cpp
@@ -1,5 +1,70 @@
-#include <stdio.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-    
+
+class Stack
+{
+private:
+    int size;
+    int *arr;
+    int top;
+
+public:
+    Stack(int s)
+    {
+        size = s;
+        top = -1;
+        arr = (int *)malloc(size * sizeof(int));
+    }
+    void push(int element)
+    {
+        if (full())
+            cout << "Stack is full" << endl;
+        else
+        {
+            top++;
+            arr[top] = element;
+        }
+    }
+
+    void pop()
+    {
+        if (empty())
+            cout << "Stack is empty" << endl;
+        else
+            top--;
+    }
+    void print()
+    {
+        for (int i = 0; i < top + 1; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
+    bool empty()
+    {
+        if (top == -1)
+            return true;
+        return false;
+    }
+    bool full()
+    {
+        if (top == size - 1)
+            return true;
+        return false;
+    }
+};
+
+int main()
+{
+    Stack s(20);
+    s.push(1);
+    s.push(2);
+    s.print();
+    s.pop();
+    s.print();
+    s.pop();
+    cout << s.empty() << endl;
+    cout << s.full() << endl;
+    s.pop();
 }
